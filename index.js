e1a97eb728a8ed10d51cb329dbe6d3d865876787
Hippasusss <dannyherbertgj@gmail.com>
2023-09-27 17:43:43 +0100

    changed from using mailchimp api to using their embedded signup widget

--- index.js
+++ index.js
@@ -1,64 +1,13 @@
 var mailInputContainer = document.getElementById('mailer');
 var bandsInTownContainer = document.getElementById('bandsintown');
 var mailThanks= document.getElementById('mailthx');
-const testKeyMailChimp = "a5a3ba0aa4d460f1488d3a7b487ef675-us17"
+const testKeyMailChimp = "01d9493d7da34e2f865e8c1354472e73-us17"
 const dcMailChimp = "us17"
 const audienceID = "44d77fc558"
 const hidden = 'hidden'
 const visible= 'visible'
 
 var emailForm = document.getElementById("emailform");
-
-async function addEmailToMailChimpDB(email) 
-{
-
-    const data = 
-        {
-            email_address: email,
-            status: 'subscribed', 
-        };
-
-    try 
-    {
-        const response = await fetch
-        (
-            `https://${dcMailChimp}.api.mailchimp.com/3.0/lists/${audienceID}/members`,
-            {
-                method: 'POST',
-                headers: 
-                {
-                    'Authorization': `Basic ${btoa(`apikey:${testKeyMailChimp}`)}`,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            }
-        );
-
-        if (response.ok) 
-        {
-            console.log('Email added to MailChimp');
-        } 
-        else 
-        {
-            console.error('Error adding email to MailChimp:', response.statusText);
-        }
-    } 
-    catch (error) 
-    {
-        console.error('Error adding email to MailChimp:', error);
-    }
-}
-
-emailForm.addEventListener("submit", (e) => 
-{
-    e.preventDefault();
-    mailThanks.style.visibility = visible;
-    let email = document.getElementById("mail");
-    console.log(email.value);
-    addEmailToMailChimpDB(email.value)
-    email.value = "";
-
-});
 
 function onLiveClick()
 {
