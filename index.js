9bd7aca4fdfbe7086adcf03cd8520c965d55787d
Hippasusss <dannyherbertgj@gmail.com>
2023-09-27 04:47:55 +0100

    mailchimp api not workign due to cors :(

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
-var mailInput = document.getElementById('mailer');
-var liveDates = document.getElementById('bandsintown');
+var mailInputContainer = document.getElementById('mailer');
+var bandsInTownContainer = document.getElementById('bandsintown');
+var mailThanks= document.getElementById('mailthx');
+const testKeyMailChimp = "a5a3ba0aa4d460f1488d3a7b487ef675-us17"
+const dcMailChimp = "us17"
+const audienceID = "44d77fc558"
 const hidden = 'hidden'
 const visible= 'visible'
+
+var emailForm = document.getElementById("emailform");
+
+async function addEmailToMailChimpDB(email) 
+{
+
+    const data = 
+        {
+            email_address: email,
+            status: 'subscribed', 
+        };
+
+    try 
+    {
+        const response = await fetch
+        (
+            `https://${dcMailChimp}.api.mailchimp.com/3.0/lists/${audienceID}/members`,
+            {
+                method: 'POST',
+                headers: 
+                {
+                    'Authorization': `Basic ${btoa(`apikey:${testKeyMailChimp}`)}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }
+        );
+
+        if (response.ok) 
+        {
+            console.log('Email added to MailChimp');
+        } 
+        else 
+        {
+            console.error('Error adding email to MailChimp:', response.statusText);
+        }
+    } 
+    catch (error) 
+    {
+        console.error('Error adding email to MailChimp:', error);
+    }
+}
+
+
+
+emailForm.addEventListener("submit", (e) => 
+{
+    e.preventDefault();
+    mailThanks.style.visibility = visible;
+    let email = document.getElementById("mail");
+    console.log(email.value);
+    addEmailToMailChimpDB(email.value)
+    email.value = "";
+
+});
+
+
+
 function onLiveClick()
 {
-    if(mailInput.style.visibility = visible)
+    if(mailInputContainer.style.visibility = visible)
     {
-        mailInput.style.visibility = hidden;
+        mailInputContainer.style.visibility = hidden;
     }
-    liveDates.style.visibility = visible;
+    bandsInTownContainer.style.visibility = visible;
 
 }
 
 function onMailClick()
 {
-    if(liveDates.style.visibility = visible)
+    if(bandsInTownContainer.style.visibility = visible)
     {
-        liveDates.style.visibility = hidden;
+        bandsInTownContainer.style.visibility = hidden;
     }
-    mailInput.style.visibility = visible;
-}
-
-input:-webkit-autofill:active{
-    -webkit-box-shadow: 0 0 0 30px white inset !important;
+    mailInputContainer.style.visibility = visible;
 }
