c1029ff4b6a298eee1cf94d9fe5b08280fb207fe
Hippasusss <dannyherbertgj@gmail.com>
2023-09-27 20:41:51 +0100

    removed refs to missing ids

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var mailInputContainer = document.getElementById('mailer');
+var mailInputContainer = document.getElementById('mc_embed_shell');
 var bandsInTownContainer = document.getElementById('bandsintown');
 var mailThanks= document.getElementById('mailthx');
-const testKeyMailChimp = "01d9493d7da34e2f865e8c1354472e73-us17"
-const dcMailChimp = "us17"
-const audienceID = "44d77fc558"
 const hidden = 'hidden'
 const visible= 'visible'
 
