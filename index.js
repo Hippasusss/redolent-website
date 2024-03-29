81c9850d501ca0f41ab522853ee9a18874955330
Hippasusss <dannyherbertgj@gmail.com>
2024-03-29 00:14:52 +0000

    Added fading in and out

--- index.js
+++ index.js
@@ -3,21 +3,44 @@ var bandsInTownContainer = document.getElementById('bandsintown');
 var mailThanks= document.getElementById('mailthx');
 const hidden = 'hidden'
 const visible= 'visible'
+const fadeTime = 10 
 
 function onLiveClick()
 {
-    if(mailInputContainer.style.visibility = visible)
-    {
-        mailInputContainer.style.visibility = hidden;
-    }
+    fade(mailInputContainer);
+    unfade(bandsInTownContainer);
     bandsInTownContainer.style.visibility = visible;
 }
 
 function onMailClick()
 {
-    if(bandsInTownContainer.style.visibility = visible)
-    {
-        bandsInTownContainer.style.visibility = hidden;
-    }
+    fade(bandsInTownContainer);
+    unfade(mailInputContainer);
     mailInputContainer.style.visibility = visible;
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, fadeTime);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, fadeTime);
 }
